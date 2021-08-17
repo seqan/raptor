43c177a948882d5ea11a71676f81ced3d6e55ff9
Enrico Seiler <enrico.seiler@hotmail.de>
2021-08-17 15:38:16 +0200

    [DOC] Add copyright

--- util/src/bash_scripts/benchmark.sh
+++ util/src/bash_scripts/benchmark.sh
@@ -1,4 +1,12 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+# -----------------------------------------------------------------------------------------------------
+# Copyright (c) 2006-2021, Knut Reinert & Freie Universität Berlin
+# Copyright (c) 2016-2021, Knut Reinert & MPI für molekulare Genetik
+# This file may be used, modified and/or redistributed under the terms of the 3-clause BSD-License
+# shipped with this file and also available at: https://github.com/seqan/raptor/blob/master/LICENSE.md
+# -----------------------------------------------------------------------------------------------------
+
 set -e
 
 READ_LENGTH=100
