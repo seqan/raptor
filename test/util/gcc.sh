0f191125f7ff0f1d79a87c1998514a9f7275618b
Enrico Seiler <enrico.seiler@hotmail.de>
2021-08-26 15:35:28 +0200

    [INFRA] Reduce test cases

---
+++ test/util/gcc.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+GCC="/usr/bin/gcc-11"
+DO_TIME=0
+
+if [[ DO_TIME -eq 0 ]]; then
+    exec "$GCC" "$@"
+else
+    FILE=$(mktemp ram_usage.XXXXXXXX)
+    exec /usr/bin/time -v "$GCC" "$@" 2> $FILE
+fi
