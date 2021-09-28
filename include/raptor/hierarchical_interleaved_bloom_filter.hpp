64c7cd7e1462e9489d9916bc53a8df9d20ab7b14
Enrico Seiler <enrico.seiler@hotmail.de>
2021-09-28 22:02:19 +0200

    [MISC] Add copyright to HIBF

--- include/raptor/hierarchical_interleaved_bloom_filter.hpp
+++ include/raptor/hierarchical_interleaved_bloom_filter.hpp
@@ -1,3 +1,10 @@
+// -----------------------------------------------------------------------------------------------------
+// Copyright (c) 2006-2021, Knut Reinert & Freie Universität Berlin
+// Copyright (c) 2016-2021, Knut Reinert & MPI für molekulare Genetik
+// This file may be used, modified and/or redistributed under the terms of the 3-clause BSD-License
+// shipped with this file and also available at: https://github.com/seqan/raptor/blob/master/LICENSE.md
+// -----------------------------------------------------------------------------------------------------
+
 #pragma once
 
 #include <seqan3/std/ranges>
@@ -484,6 +491,6 @@ public:
     [[nodiscard]] seqan3::counting_vector<value_t> const & bulk_count(value_range_t && values, size_t const threshold = 1u) && noexcept = delete;
     //!\}
 };
-#endif
+#endif // RAPTOR_HIBF_HAS_COUNT
 
 } // namespace raptor
