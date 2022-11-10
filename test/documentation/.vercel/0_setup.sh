5127803d8a632bede085a9f24e83bf17a2ead03c
Enrico Seiler <enrico.seiler@hotmail.de>
2022-11-10 12:20:28 +0100

    [DOC] Add comments for vercel

--- test/documentation/.vercel/0_setup.sh
+++ test/documentation/.vercel/0_setup.sh
@@ -1,36 +1,48 @@
 #!/usr/bin/env bash
-set -exo pipefail
+set -euxo pipefail
 
 DOXYGEN_VERSION=1.9.5
 WORK_DIR=`pwd`
-CACHE_DIR="${WORK_DIR}/node_modules"
+CACHE_DIR="${WORK_DIR}/node_modules" # The node_modules directory is always cached.
 
-# Install dependencies.
+### Install dependencies.
+# Enables EPEL 7 which hosts xapian-core-devel. The server runs on Amazon Linux 2.
 amazon-linux-extras install epel &>/dev/null
 yum --assumeyes --quiet install wget cmake3 flex bison xz graphviz xapian-core-devel &>/dev/null
 
-# Download doxygen.
+### Download doxygen.
 mkdir -p ${CACHE_DIR}/doxygen-download
 wget --retry-connrefused --waitretry=30 --read-timeout=30 --timeout=30 --tries=20 --no-clobber --quiet --directory-prefix=${CACHE_DIR}/doxygen-download/ https://sourceforge.net/projects/doxygen/files/rel-${DOXYGEN_VERSION}/doxygen-${DOXYGEN_VERSION}.src.tar.gz
 tar -C ${CACHE_DIR}/doxygen-download -zxf ${CACHE_DIR}/doxygen-download/doxygen-${DOXYGEN_VERSION}.src.tar.gz
 
-# Configure doxygen.
+### Configure doxygen.
 cd ${CACHE_DIR}/doxygen-download/doxygen-${DOXYGEN_VERSION}
 mkdir -p build && cd build
+
+# xapian-core-devel from epel (EPEL 7) is built with pre-cxx11-abi, so we need to do so for doxygen, too.
+# -Dbuild_search=ON enables building doxyindexer and doxysearch.cgi
 cmake_command() {
-    cmake3 -Dbuild_search=ON -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_FLAGS="-D_GLIBCXX_USE_CXX11_ABI=0 -w" -G "Unix Makefiles" .. 1>/dev/null
+    cmake3 .. \
+        -Dbuild_search=ON \
+        -DCMAKE_BUILD_TYPE=Release \
+        -DCMAKE_CXX_FLAGS="-D_GLIBCXX_USE_CXX11_ABI=0 -w" \
+        -G "Unix Makefiles" 1>/dev/null
 }
+
+# If CMake fails, we clear the directory and try again. Might happen due to path changes on vercel's side.
 if ! cmake_command; then
     rm -rf *
     cmake_command
 fi
 
-# Build doxygen.
-make -j 4 1>/dev/null
+### Build doxygen.
+make -j4 1>/dev/null
 
-# Install doxygen
+### Install doxygen.
 make install DESTDIR=${CACHE_DIR}/doxygen-${DOXYGEN_VERSION} 1>/dev/null
 
-# Symlink doxygen.
+### Symlink.
+# Just allows using an easier path.
 ln -s ${CACHE_DIR}/doxygen-${DOXYGEN_VERSION}/usr/local/bin ${WORK_DIR}/doxygen-bin
+# We will need to ship the dynamically linked libxapian. Copied for easy access in api/doxysearch.sh
 cp /usr/lib64/libxapian.so ${WORK_DIR}/doxygen-bin
--- test/documentation/.vercel/api/doxysearch.sh
+++ test/documentation/.vercel/api/doxysearch.sh
@@ -1,15 +1,20 @@
 import "querystring@1.3.0"
 
 build() {
+    # Every file generated inside the working directory during build() will be bundled.
+    # We need the binary.
     cp ${WORK_PATH}/doxygen-bin/doxysearch.cgi .
+    # We need the dynamically linked library.
     cp ${WORK_PATH}/doxygen-bin/libxapian.so libxapian.so.22
+    # We need the index.
     cp -r ${WORK_PATH}/doxygen-bin/doxysearch.db .
 }
 
+# Extracts the querystring and calls doxysearch.
+# Doxysearch returns a content-header that must be removed (tail -n +3).
 handler() {
-    local path
-    path="$(jq -r '.path' < "$1")"
-    string=$(querystring "$path" | querystring_unescape)
+    local path="$(jq -r '.path' < "$1")"
+    local string=$(querystring "$path" | querystring_unescape)
     export LD_LIBRARY_PATH="$(pwd):/lib64"
     ./doxysearch.cgi "$string" | tail -n +3
 }
