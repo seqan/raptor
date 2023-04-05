155ba73c619606dd2761e3f638f6a440f29602e2
Svenja Mehringer <svenja.mehringer@fu-berlin.de>
2023-04-05 08:01:51 +0200

    [MISC] Resolve circular include of hierarchical_build.hpp and initialise_max_bin_kmers.hpp

--- src/build/hibf/hierarchical_build.cpp
+++ src/build/hibf/hierarchical_build.cpp
@@ -15,7 +15,6 @@
 #include <raptor/build/hibf/compute_kmers.hpp>
 #include <raptor/build/hibf/construct_ibf.hpp>
 #include <raptor/build/hibf/hierarchical_build.hpp>
-#include <raptor/build/hibf/initialise_max_bin_kmers.hpp>
 #include <raptor/build/hibf/insert_into_ibf.hpp>
 #include <raptor/build/hibf/loop_over_children.hpp>
 #include <raptor/build/hibf/update_user_bins.hpp>
@@ -37,6 +36,34 @@ size_t hierarchical_build(robin_hood::unordered_flat_set<size_t> & parent_kmers,
     std::vector<int64_t> ibf_positions(current_node_data.number_of_technical_bins, ibf_pos);
     std::vector<int64_t> filename_indices(current_node_data.number_of_technical_bins, -1);
     robin_hood::unordered_flat_set<size_t> kmers{};
+
+    auto initialise_max_bin_kmers = [] (robin_hood::unordered_flat_set<size_t> & kmers,
+                                        std::vector<int64_t> & ibf_positions,
+                                        std::vector<int64_t> & filename_indices,
+                                        lemon::ListDigraph::Node const & node,
+                                        build_data<data_layout_mode> & data,
+                                        build_arguments const & arguments)
+        -> size_t
+    {
+        auto & node_data = data.node_map[node];
+
+        if (node_data.favourite_child != lemon::INVALID) // max bin is a merged bin
+        {
+            // recursively initialize favourite child first
+            ibf_positions[node_data.max_bin_index] =
+                hierarchical_build(kmers, node_data.favourite_child, data, arguments, false);
+            return 1;
+        }
+        else // max bin is not a merged bin
+        {
+            // we assume that the max record is at the beginning of the list of remaining records.
+            auto const & record = node_data.remaining_records[0];
+            compute_kmers(kmers, arguments, record);
+            update_user_bins(data, filename_indices, record);
+
+            return record.number_of_bins.back();
+        }
+    };
 
     // initialize lower level IBF
     size_t const max_bin_tbs =
