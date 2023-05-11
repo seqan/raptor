2d3fc79cd2aa43d9e0912b70c34bcaad567f95fc
smehringer <svenja.mehringer@gmail.com>
2023-05-11 12:10:00 +0200

    [MISC] Remove create_ibfs_from_chopper_pack

--- src/build/hibf/chopper_build.cpp
+++ src/build/hibf/chopper_build.cpp
@@ -12,8 +12,16 @@
 
 #include <lemon/list_graph.h> /// Must be first include.
 
+#include <robin_hood.h>
+
+#include <chopper/layout/compute_fp_correction.hpp>
+#include <chopper/layout/layout.hpp>
+
+#include <raptor/build/hibf/build_data.hpp>
 #include <raptor/build/hibf/chopper_build.hpp>
-#include <raptor/build/hibf/create_ibfs_from_chopper_pack.hpp>
+#include <raptor/build/hibf/hierarchical_build.hpp>
+#include <raptor/build/hibf/initialise_build_tree.hpp>
+#include <raptor/build/hibf/read_chopper_pack_file.hpp>
 #include <raptor/build/store_index.hpp>
 #include <raptor/index.hpp>
 
@@ -22,15 +30,33 @@ namespace raptor::hibf
 
 void chopper_build(build_arguments const & arguments)
 {
-    build_data data{.arguments = arguments};
+    std::vector<std::vector<std::string>> filenames;
 
-    create_ibfs_from_chopper_pack(data);
+    chopper::layout::layout hibf_layout = read_chopper_pack_file(filenames, arguments.bin_file);
+
+    build_data data{.arguments = arguments, .filenames = filenames};
+
+    size_t const number_of_ibfs = hibf_layout.max_bins.size() + 1;
+    data.hibf.ibf_vector.resize(number_of_ibfs);
+    data.hibf.user_bins.set_ibf_count(number_of_ibfs);
+    data.hibf.user_bins.set_user_bin_count(hibf_layout.user_bins.size());
+    data.hibf.next_ibf_id.resize(number_of_ibfs);
+
+    initialise_build_tree(hibf_layout, data.ibf_graph, data.node_map);
+
+    lemon::ListDigraph::Node root = data.ibf_graph.nodeFromId(0); // root node = high level IBF node
+    robin_hood::unordered_flat_set<size_t> root_kmers{};
+
+    size_t const t_max{data.node_map[root].number_of_technical_bins};
+    data.fp_correction = chopper::layout::compute_fp_correction(arguments.fpr, arguments.hash, t_max);
+
+    hierarchical_build(root_kmers, root, data, true);
 
     arguments.index_allocation_timer.start();
     raptor_index<hierarchical_interleaved_bloom_filter> index{window{arguments.window_size},
                                                               arguments.shape,
                                                               arguments.parts,
-                                                              data.filenames,
+                                                              filenames,
                                                               arguments.fpr,
                                                               std::move(data.hibf)};
     arguments.index_allocation_timer.stop();
