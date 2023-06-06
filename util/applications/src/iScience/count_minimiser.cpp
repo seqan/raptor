6b78e600ae726eee587e518d87beb1bcb06d8613
Enrico Seiler <enrico.seiler@hotmail.de>
2023-06-06 17:31:50 +0200

    [UTIL] Update count_minimiser

--- util/applications/src/iScience/count_minimiser.cpp
+++ util/applications/src/iScience/count_minimiser.cpp
@@ -14,14 +14,13 @@
 #include <sharg/all.hpp>
 
 #include <seqan3/core/algorithm/detail/execution_handler_parallel.hpp>
-#include <seqan3/search/views/minimiser_hash.hpp>
-#include <seqan3/utility/views/chunk.hpp>
 
-#include <raptor/adjust_seed.hpp>
 #include <raptor/argument_parsing/parse_bin_path.hpp>
 #include <raptor/argument_parsing/validators.hpp>
 #include <raptor/contrib/std/chunk_view.hpp>
-#include <raptor/dna4_traits.hpp>
+#include <raptor/contrib/std/join_with_view.hpp>
+#include <raptor/contrib/std/zip_view.hpp>
+#include <raptor/file_reader.hpp>
 
 struct config
 {
@@ -36,36 +35,21 @@ struct config
 
 void compute_minimisers(config const & cfg)
 {
-    using sequence_file_t = seqan3::sequence_file_input<raptor::dna4_traits, seqan3::fields<seqan3::field::seq>>;
-    auto minimiser_view = seqan3::views::minimiser_hash(seqan3::ungapped{cfg.kmer_size},
-                                                        seqan3::window_size{cfg.window_size},
-                                                        seqan3::seed{raptor::adjust_seed(cfg.kmer_size)});
-
-    std::vector<uint64_t> minimiser_counts;
-    minimiser_counts.reserve(cfg.bin_path.size());
+    std::vector<uint64_t> minimiser_counts(cfg.bin_path.size());
     robin_hood::unordered_node_set<uint64_t> all_minimiser_set{};
-    std::mutex push_back_mutex;
     std::mutex merge_mutex;
 
-    auto worker = [&](auto && file_range, auto &&)
+    auto worker = [&](auto && zipped_view, auto &&)
     {
         robin_hood::unordered_flat_set<uint64_t> minimiser_set{};
+        raptor::file_reader<raptor::file_types::sequence> reader{seqan3::ungapped{cfg.kmer_size}, cfg.window_size};
 
-        for (auto && file_names : file_range)
+        for (auto && [file_names, bin_number] : zipped_view)
         {
-            for (auto && file_name : file_names)
-            {
-                for (auto & [seq] : sequence_file_t{file_name})
-                {
-                    auto hash_view = seq | minimiser_view | std::views::common;
-                    minimiser_set.insert(hash_view.begin(), hash_view.end());
-                }
-            }
+            reader.hash_into(file_names, std::inserter(minimiser_set, minimiser_set.begin()));
 
-            {
-                std::lock_guard const lock{push_back_mutex};
-                minimiser_counts.emplace_back(minimiser_set.size());
-            }
+            minimiser_counts[bin_number] = minimiser_set.size();
+
             {
                 std::lock_guard const lock{merge_mutex};
                 all_minimiser_set.insert(minimiser_set.begin(), minimiser_set.end());
@@ -75,15 +59,24 @@ void compute_minimisers(config const & cfg)
         }
     };
 
-    size_t const chunk_size = std::ceil<size_t>(cfg.bin_path.size() / cfg.threads);
-    auto chunked_view = cfg.bin_path | seqan::std::views::chunk(chunk_size);
+    size_t const chunk_size = std::max<size_t>(1, std::floor(cfg.bin_path.size() / static_cast<double>(cfg.threads)));
+    auto chunked_view =
+        seqan::std::views::zip(cfg.bin_path, std::views::iota(0u)) | seqan::std::views::chunk(chunk_size);
     seqan3::detail::execution_handler_parallel executioner{cfg.threads};
     executioner.bulk_execute(worker, std::move(chunked_view), []() {});
 
     std::ofstream output{cfg.out_path.string()};
-    output << all_minimiser_set.size() << '\n';
-    for (auto const & count : minimiser_counts)
+    output << "# k-mer size: " << static_cast<size_t>(cfg.kmer_size) << '\n';
+    output << "# Window size: " << cfg.window_size << '\n';
+    output << "# Unique k-mers across all: " << all_minimiser_set.size() << '\n';
+    output << "# File\tUnique k-mer count\n";
+    for (auto && [file_names, count] : seqan::std::views::zip(cfg.bin_path, minimiser_counts))
+    {
+        for (auto && elem : seqan::std::views::join_with(file_names, ' '))
+            output << elem;
+        output << '\t';
         output << count << '\n';
+    }
 }
 
 int main(int argc, char ** argv)
