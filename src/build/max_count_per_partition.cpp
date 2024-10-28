e30a278146b3d890e08b90fd36b0babd108dd333
Enrico Seiler <enrico.seiler@hotmail.de>
2024-10-28 18:46:55 +0100

    [MISC] Use sketches for max bin estimation, then use exact count

--- src/build/max_count_per_partition.cpp
+++ src/build/max_count_per_partition.cpp
@@ -10,6 +10,7 @@
 #include <algorithm>
 #include <fstream>
 
+#include <hibf/contrib/robin_hood.hpp>
 #include <hibf/sketch/hyperloglog.hpp>
 
 #include <raptor/build/max_count_per_partition.hpp>
@@ -30,38 +31,68 @@ std::vector<size_t> max_count_per_partition(partition_config const & cfg,
                                             uint32_t const window_size)
 {
     std::vector<size_t> kmers_per_partition(cfg.partitions);
+    std::vector<size_t> bin_per_partition(cfg.partitions);
     std::mutex callback_mutex{};
     file_reader<file_type> const reader{shape, window_size};
 
-    auto callback = [&callback_mutex, &kmers_per_partition, &cfg](std::vector<size_t> const & kmer_counts)
+    auto callback = [&](std::vector<size_t> const & kmer_counts, std::vector<size_t> const & bin_ids)
     {
         std::lock_guard<std::mutex> guard{callback_mutex};
         for (size_t i = 0; i < cfg.partitions; ++i)
-            kmers_per_partition[i] = std::max<size_t>(kmers_per_partition[i], kmer_counts[i]);
+        {
+            if (kmer_counts[i] > kmers_per_partition[i])
+            {
+                kmers_per_partition[i] = kmer_counts[i];
+                bin_per_partition[i] = bin_ids[i];
+            }
+        }
     };
 
     auto worker = [&callback, &reader, &cfg](auto && zipped_view)
     {
         std::vector<size_t> max_kmer_counts(cfg.partitions);
-        std::vector<seqan::hibf::sketch::hyperloglog> kmer_counts(cfg.partitions, 15u);
+        std::vector<size_t> max_bin_ids(cfg.partitions);
+        std::vector<seqan::hibf::sketch::hyperloglog> sketches(cfg.partitions, 15u);
 
         for (auto && [file_names, bin_number] : zipped_view)
         {
             reader.for_each_hash(file_names,
                                  [&](auto && hash)
                                  {
-                                     kmer_counts[cfg.hash_partition(hash)].add(hash);
+                                     sketches[cfg.hash_partition(hash)].add(hash);
                                  });
             for (size_t i = 0; i < cfg.partitions; ++i)
             {
-                max_kmer_counts[i] = std::max<size_t>(max_kmer_counts[i], kmer_counts[i].estimate());
-                kmer_counts[i].reset();
+                size_t const estimate = sketches[i].estimate();
+                if (estimate > max_kmer_counts[i])
+                {
+                    max_kmer_counts[i] = estimate;
+                    max_bin_ids[i] = bin_number;
+                }
+                sketches[i].reset();
             }
         }
-        callback(max_kmer_counts);
+        callback(max_kmer_counts, max_bin_ids);
     };
 
+    // Use sketches to determine biggest bin.
     call_parallel_on_bins(worker, bin_path, threads);
+
+    // Get exact count for biggest bin. Sketch estimate's accuracy depends on sketch_bits (here: 15).
+    robin_hood::unordered_flat_set<uint64_t> kmers{};
+#pragma omp parallel for schedule(dynamic) num_threads(threads) private(kmers)
+    for (size_t i = 0; i < cfg.partitions; ++i)
+    {
+        kmers.clear();
+        auto insert_it = std::inserter(kmers, kmers.end());
+        reader.hash_into_if(bin_path[bin_per_partition[i]],
+                            insert_it,
+                            [&](uint64_t const hash)
+                            {
+                                return cfg.hash_partition(hash) == i;
+                            });
+        kmers_per_partition[i] = kmers.size();
+    }
 
     return kmers_per_partition;
 }
--- test/unit/api/compute_bin_size.cpp
+++ test/unit/api/compute_bin_size.cpp
@@ -21,21 +21,21 @@ TEST_F(compute_bin_size, multiple_files)
 {
     raptor::build_arguments const config{.bin_path = {{data("bin1.fa"), data("bin2.fa")}}};
     size_t const result = raptor::compute_bin_size(config);
-    EXPECT_EQ(result, 3801u); // exact count = 480, exact size = 3794
+    EXPECT_EQ(result, 3794u); // exact count = 480, exact size = 3794
 }
 
 TEST_F(compute_bin_size, multiple_records)
 {
     raptor::build_arguments const config{.bin_path = {{data("multi_record_bin.fa")}}};
     size_t const result = raptor::compute_bin_size(config);
-    EXPECT_EQ(result, 3801u); // exact count = 480, exact size = 3794
+    EXPECT_EQ(result, 3794u); // exact count = 480, exact size = 3794
 }
 
 TEST_F(compute_bin_size, multiple_files_multiple_records)
 {
     raptor::build_arguments const config{.bin_path = {{data("multi_record_bin.fa"), data("multi_record_bin.fa")}}};
     size_t const result = raptor::compute_bin_size(config);
-    EXPECT_EQ(result, 3801u); // exact count = 480, exact size = 3794
+    EXPECT_EQ(result, 3794u); // exact count = 480, exact size = 3794
 }
 
 TEST_F(compute_bin_size, mixed)
@@ -43,5 +43,5 @@ TEST_F(compute_bin_size, mixed)
     raptor::build_arguments const config{
         .bin_path = {{data("multi_record_bin.fa")}, {data("bin3.fa")}, {data("bin4.fa")}}};
     size_t const result = raptor::compute_bin_size(config);
-    EXPECT_EQ(result, 3801u); // exact count = 480, exact size = 3794
+    EXPECT_EQ(result, 3794u); // exact count = 480, exact size = 3794
 }
